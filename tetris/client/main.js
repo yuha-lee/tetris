362fc06ba43e0617cafdd5977dbe67fc744e9ecb
yuha_lee <yuha.lee.dev@gmail.com>
2019-12-31 20:17:10 +0900

    tetris/add websocket

---
+++ tetris/client/main.js
@@ -0,0 +1,32 @@
+const tetrisManager = new TetrisManager(document);
+const localTetris = tetrisManager.createPlayer();
+
+const connectionManager = new ConnectionManager();
+connectionManager.connect('ws://localhost:9000');
+
+const keyListener = (event) => {
+    [
+        [37, 39, 32, 90, 88, 40],
+        [74, 76, 68, 65, 83, 75]
+    ].forEach((key, index) => {
+        const player = localTetris.player;
+        if (event.type === 'keydown') {
+            if (event.keyCode === key[0]) {
+                player.move(-1);    // left arrow
+            } else if (event.keyCode === key[1]) {
+                player.move(1);     // right arrow
+            } else if (event.keyCode === key[2]) {
+                player.drop(0);     // drop to the bottom
+            } else if (event.keyCode === key[3]) {
+                player.rotate(-1);  // rotate left
+            } else if (event.keyCode === key[4]) {
+                player.rotate(1);   // roate right
+            } else if (event.keyCode === key[5]) {
+                player.drop(1);     //down arrow
+            }
+        }
+    });
+};
+
+document.addEventListener('keydown', keyListener);
+//document.addEventListener('keyup', keyListener);
