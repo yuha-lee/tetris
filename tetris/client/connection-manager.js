08b737be67f19b21c013171aea851f1e3bfa56e6
yuha_lee <yuha.lee.dev@gmail.com>
2020-01-01 20:03:22 +0900

    tetris/manage sessions

--- tetris/client/connection-manager.js
+++ tetris/client/connection-manager.js
@@ -1,12 +1,67 @@
 class ConnectionManager
 {
-    constructor()
+    constructor(tetrisManager)
     {
         this.conn = null;
+        this.peers = new Map;
+        this.tetrisManager = tetrisManager;
     }
 
     connect(address)
     {
         this.conn = new WebSocket(address);
+        this.conn.addEventListener('open', () => {
+            console.log('Connection established');
+            this.initSession();
+        });
+
+        this.conn.addEventListener('message', event => {
+            console.log('Received message', event.data);
+            this.receive(event.data);
+        })
+    }
+
+    initSession()
+    {
+        const sessionId = window.location.hash.split('#')[1];
+        if (sessionId) {
+            this.send({
+                type: 'join-session',
+                id: sessionId
+            });
+        } else {
+            this.send({
+                type: 'create-session',
+            });
+        }
+    }
+
+    updateManager(peers)
+    {
+        const me = peers.you;
+        const clients = peers.clients.filter(id => me !== id);
+        clients.forEach(id => {
+            if (!this.peers.has(id)) {
+                const tetris = this.tetrisManager.createPlayer();
+                this.peers.set(id, tetris);
+            }
+        });
+    }
+
+    receive(msg)
+    {
+        const data = JSON.parse(msg);
+        if (data.type === 'session-created') {
+            window.location.hash = data.id;
+        } else if (data.type === 'session-broadcast') {
+            this.updateManager(data.peers);
+        }
+    }
+
+    send(data)
+    {
+        const msg = JSON.stringify(data);
+        console.log(`Sending message ${msg}`);
+        this.conn.send(msg);
     }
 }
