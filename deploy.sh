45fe6adddab81770200e85bc4a961ed82a8acede
Yuha Lee <yuha.lee.dev@gmail.com>
2021-03-09 21:49:34 +0900

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -112,7 +112,7 @@ selectNodeVersion
 # 3. Install npm packages
 if [ -e "$DEPLOYMENT_TARGET/package.json" ]; then
   cd "$DEPLOYMENT_TARGET"
-  echo "Running $NPM_CMD install --production"
+  #echo "Running $NPM_CMD install --production"
   #eval $NPM_CMD install --production
   #eval npm update
   eval npm config set strict-ssl false
