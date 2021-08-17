4f1fa7dc420751c8e02e141bbcc1a9b67cb13fdb
Yuha Lee <yuha.lee.dev@gmail.com>
2021-08-17 22:17:44 +0900

    no message

--- server.js
+++ server.js
@@ -2,10 +2,7 @@ const express = require('express');
 const { Server } = require('ws');
 
 const PORT = process.env.PORT || 3000;
-const INDEX = '/index.html';
-
 const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const ws = new Server({ server });
